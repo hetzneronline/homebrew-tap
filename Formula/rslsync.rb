7acc88eb19a8f11d0d9636a2276aefd6c62131b5
Noah Seefried <noah.seefried@hetzner.com>
2019-03-22 14:45:21 +0100

    Add rslsync formula

---
+++ Formula/rslsync.rb
@@ -0,0 +1,11 @@
+class Rslsync < Formula
+  desc "Resilio Sync is a proprietary file sharing system that relies on the BitTorrent protocol."
+  homepage "https://www.resilio.com/"
+  url "https://download-cdn.resilio.com/stable/linux-x64/resilio-sync_x64.tar.gz"
+  sha256 "9f6adeaea9a6bdbdf232bc585929816aeeeb3bd654252cabd99f1edb753b9384"
+  version "2.6.3"
+
+  def install
+    bin.install "rslsync"
+  end
+end
