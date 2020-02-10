7a074fb8ebcd7712ac498a9fd4de3df93c46a5de
Noah Seefried <noah.seefried@hetzner.com>
2020-02-10 12:58:38 +0100

    Add mongodb formula

---
+++ Formula/mongodb.rb
@@ -0,0 +1,17 @@
+#
+# Copyright 2019 Hetzner Online GmbH
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+class Mongodb < Formula
+  desc "MongoDB is a document-oriented database."
+  homepage "https://www.mongodb.com/"
+  url "https://downloads.mongodb.org/linux/mongodb-linux-x86_64-debian10-4.3.3.tgz"
+  sha256 "541e8a300ba0374e31232090fcb6c3167733eeeb8fcce31b69bbf9dc384362d3"
+  version "4.3.3"
+
+  def install
+    bin.install "mongodb"
+  end
+end
---
+++ Formula/mongodb@9.rb
@@ -0,0 +1,17 @@
+#
+# Copyright 2019 Hetzner Online GmbH
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+class MongodbAT9 < Formula
+  desc "MongoDB is a document-oriented database."
+  homepage "https://www.mongodb.com/"
+  url "https://downloads.mongodb.org/linux/mongodb-linux-x86_64-debian92-4.3.3.tgz"
+  sha256 "78972c295d28977fdba11cda787e8564038ae758f883fd47421e98b7dd727474"
+  version "4.3.3"
+
+  def install
+    bin.install "mongodb"
+  end
+end
