5432ec3363de715ea276c841d21b0dd30d93d456
Noah <dev@noah-seefried.de>
2020-05-15 14:06:14 +0200

    Update rslsync to 2.7

--- Formula/rslsync.rb
+++ Formula/rslsync.rb
@@ -8,8 +8,8 @@ class Rslsync < Formula
   desc "Resilio Sync is a proprietary file sharing system that relies on the BitTorrent protocol."
   homepage "https://www.resilio.com/"
   url "https://download-cdn.resilio.com/stable/linux-x64/resilio-sync_x64.tar.gz"
-  sha256 "4891d7fa33bfe00a6ff907ef3e0fa601647105fa318a53a53f1795a7a49e3eb0"
-  version "2.6.4"
+  sha256 "ce1415403e089350bf6966af08d5822defd3e6d4a1cb902abc28d6f7fc247c9f"
+  version "2.7"
 
   def install
     bin.install "rslsync"
