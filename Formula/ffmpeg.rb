533e560995efc3ec3682cc26c80b51340cb29030
Noah Seefried <noah.seefried@hetzner.com>
2020-06-22 15:24:56 +0200

    Update ffmpeg to 4.3

--- Formula/ffmpeg.rb
+++ Formula/ffmpeg.rb
@@ -7,9 +7,9 @@
 class Ffmpeg < Formula
   desc "FFmpeg is a free software project that produces libraries and programs for handling multimedia data."
   homepage "https://ffmpeg.org/"
-  url "https://github.com/hetzneronline/homebrew-packages/raw/master/ffmpeg/ffmpeg-4.1.3-amd64-static.tar.gz"
-  sha256 "e32eb52386cfcbd18e13aa25165d178ee1d9c9f516e5ad2cbc445a4c40ed8891"
-  version "4.1.3"
+  url "https://github.com/hetzneronline/homebrew-packages/raw/master/ffmpeg/ffmpeg-4.3-amd64-static.tar.gz"
+  sha256 "169adb3972c94a3f6516fa5775666f69fc2db48b144ffb06bae23b55b0cc0e84"
+  version "4.3"
 
   def install
     bin.install "ffmpeg"
