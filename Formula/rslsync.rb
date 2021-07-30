e7480d590f8ef2dcaa58644522e88ee537c4e450
Noah Seefried <noah.seefried@hetzner.com>
2021-07-30 13:32:40 +0200

    Use mirrored archive for rslsync

--- Formula/rslsync.rb
+++ Formula/rslsync.rb
@@ -7,9 +7,8 @@
 class Rslsync < Formula
   desc "Resilio Sync is a proprietary file sharing system that relies on the BitTorrent protocol."
   homepage "https://www.resilio.com/"
-  url "https://download-cdn.resilio.com/stable/linux-x64/resilio-sync_x64.tar.gz"
+  url "https://software-installer.hetzner.com/mirror/rslsync/2.7.2/resilio-sync_x64.tar.gz"
   sha256 "0e9e92eb0deeb9314e9676275e91fdce6d6296b352637e7dc59eecb03f2d593d"
-  version "2.7.2"
 
   def install
     bin.install "rslsync"
