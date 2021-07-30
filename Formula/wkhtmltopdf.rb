8f5034f9c5c8c4f5a3f95245eba0361fded2baf7
Noah Seefried <noah.seefried@hetzner.com>
2021-07-30 13:31:44 +0200

    Update wkhtmltopdf to 0.12.6

--- Formula/wkhtmltopdf.rb
+++ Formula/wkhtmltopdf.rb
@@ -7,9 +7,8 @@
 class Wkhtmltopdf < Formula
   desc "Command line tool to render HTML into PDF and various image formats using the Qt WebKit rendering engine."
   homepage "https://wkhtmltopdf.org/"
-  url "https://github.com/hetzneronline/homebrew-packages/raw/master/wkhtmltopdf/wkhtmltopdf-0.12.5-1.tar.gz"
-  sha256 "a3dab1b55e5be50076d35778d9e0aa8f8fc14c26ed4f1853e0a590860a6015b7"
-  version "0.12.5-1"
+  url "https://software-installer.hetzner.com/mirror/wkhtmltopdf/0.12.6/wkhtmltox_0.12.6-1_amd64.tar.gz"
+  sha256 "bc8b8c802941c8e37fdee6a418cfd11d7f5e4537029b145e88b76016329e335f"
 
   def install
     bin.install "wkhtmltopdf"
