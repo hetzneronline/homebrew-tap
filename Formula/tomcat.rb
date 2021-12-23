eadd4c91f74fad53fbb37a6db31e8d208bd31f07
rook59 <34218225+rook59@users.noreply.github.com>
2021-12-23 11:21:25 +0100

    Add own tomcat formula
    Remove bottle, remove openjdk dependency, remove service

---
+++ Formula/tomcat.rb
@@ -0,0 +1,46 @@
+class Tomcat < Formula
+  desc "Implementation of Java Servlet and JavaServer Pages"
+  homepage "https://tomcat.apache.org/"
+  url "https://www.apache.org/dyn/closer.lua?path=tomcat/tomcat-10/v10.0.14/bin/apache-tomcat-10.0.14.tar.gz"
+  mirror "https://archive.apache.org/dist/tomcat/tomcat-10/v10.0.14/bin/apache-tomcat-10.0.14.tar.gz"
+  sha256 "d64b9a538d645a7d43cef5143983dd010dc99e49151a8c1c21ee75cd82dadbea"
+  license "Apache-2.0"
+
+  def install
+    # Remove Windows scripts
+    rm_rf Dir["bin/*.bat"]
+
+    # Install files
+    prefix.install %w[NOTICE LICENSE RELEASE-NOTES RUNNING.txt]
+
+    pkgetc.install Dir["conf/*"]
+    (buildpath/"conf").rmdir
+    libexec.install_symlink pkgetc => "conf"
+
+    libexec.install Dir["*"]
+    (bin/"catalina").write_env_script "#{libexec}/bin/catalina.sh", JAVA_HOME: Formula["openjdk"].opt_prefix
+  end
+
+  def caveats
+    <<~EOS
+      Configuration files: #{pkgetc}
+    EOS
+  end
+
+  test do
+    ENV["CATALINA_BASE"] = testpath
+    cp_r Dir["#{libexec}/*"], testpath
+    rm Dir["#{libexec}/logs/*"]
+
+    pid = fork do
+      exec bin/"catalina", "start"
+    end
+    sleep 3
+    begin
+      system bin/"catalina", "stop"
+    ensure
+      Process.wait pid
+    end
+    assert_predicate testpath/"logs/catalina.out", :exist?
+  end
+end
