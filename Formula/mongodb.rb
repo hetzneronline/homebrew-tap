749b16d7d7d40db68292ede183101c6fb072b055
rook59 <34218225+rook59@users.noreply.github.com>
2022-01-13 11:18:53 +0100

    Update mongodb to 5.0.5

--- Formula/mongodb.rb
+++ Formula/mongodb.rb
@@ -7,8 +7,8 @@
 class Mongodb < Formula
   desc "MongoDB is a document-oriented database."
   homepage "https://www.mongodb.com/"
-  url "https://software-installer.hetzner.com/mirror/mongodb/5.0.4/mongodb-linux-x86_64-debian10-5.0.4.tgz"
-  sha256 "1c8b7ae5c8f3bdcd47621ab615489db64b8ab8e1643a88edb3ab2992670908d4"
+  url "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-debian10-5.0.5.tgz"
+  sha256 "62363b7c608e9f6ae353fe426c1d7c43b0e3c421a982fc1a105dd6ad605dda10"
 
   def install
     bin.install "bin/mongod"
