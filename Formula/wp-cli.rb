94f2bce169d06821abf783e69b63cd7b211b6681
rook59 <34218225+rook59@users.noreply.github.com>
2022-11-02 14:27:16 +0100

    Update wp-cli to 2.7.1

--- Formula/wp-cli.rb
+++ Formula/wp-cli.rb
@@ -7,8 +7,8 @@
 class WpCli < Formula
   desc "Command-line interface for WordPress"
   homepage "https://wp-cli.org/"
-  url "https://github.com/wp-cli/wp-cli/releases/download/v2.6.0/wp-cli-2.6.0.phar"
-  sha256 "d166528cab60bc8229c06729e7073838fbba68d6b2b574504cb0278835c87888"
+  url "https://github.com/wp-cli/wp-cli/releases/download/v2.7.1/wp-cli-2.7.1.phar"
+  sha256 "bbf096bccc6b1f3f1437e75e3254f0dcda879e924bbea403dff3cfb251d4e468"
   license "MIT"
 
   livecheck do
