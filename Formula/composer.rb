7b0357a5b1507e27b548903b22be599610e4ac70
rook59 <34218225+rook59@users.noreply.github.com>
2023-03-08 14:43:04 +0100

    Update composer to 2.5.4

--- Formula/composer.rb
+++ Formula/composer.rb
@@ -7,8 +7,8 @@
 class Composer < Formula
   desc "Dependency Manager for PHP"
   homepage "https://getcomposer.org/"
-  url "https://getcomposer.org/download/2.5.1/composer.phar"
-  sha256 "f1b94fee11a5bd6a1aae5d77c8da269df27c705fcc806ebf4c8c2e6fa8645c20"
+  url "https://getcomposer.org/download/2.5.4/composer.phar"
+  sha256 "91ce6cbf9463eae86ae9d5c21d42faa601a519f3fbb2b623a55ee24678079bd3"
   license "MIT"
 
   def install
